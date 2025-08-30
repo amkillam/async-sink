7f3cdd5b2e30225a1016932129c62b309914cf22
Adam Killam <adammkillam@gmail.com>
2025-08-30 05:13:10 -0230

    WIP unit tests

---
+++ tests/sink_fanout.rs
@@ -0,0 +1,22 @@
+use tokio::sync::mpsc;
+use tokio_sink::SinkExt;
+use tokio_stream::{self, StreamExt};
+
+#[tokio::test]
+async fn it_works() {
+    let (tx1, rx1) = mpsc::channel(1);
+    let (tx2, rx2) = mpsc::channel(2);
+    let tx = tx1.fanout(tx2).sink_map_err(|_| ());
+
+    let src = stream::iter((0..10).map(Ok));
+    let fwd = src.forward(tx);
+
+    let collect_fut1 = rx1.collect::<Vec<_>>();
+    let collect_fut2 = rx2.collect::<Vec<_>>();
+    let (_, vec1, vec2) = join!(fwd, collect_fut1, collect_fut2).await;
+
+    let expected = (0..10).map(Ok::<_, ()>).collect::<Vec<_>>();
+
+    assert_eq!(vec1, expected);
+    assert_eq!(vec2, expected);
+}
